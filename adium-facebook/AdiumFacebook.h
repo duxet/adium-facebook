4cf6d128893214abf550e7e05160960fea31467b
duxet <duxetlg@gmail.com>
2015-10-06 22:47:02 +0200

    Add Facebook protocol code

---
+++ adium-facebook/AdiumFacebook.h
@@ -0,0 +1,14 @@
+//
+//  AdiumFacebook.h
+//  adium-facebook
+//
+//  Created by duxet on 08/08/15.
+//  Copyright (c) 2015 duxet. All rights reserved.
+//
+
+#import <Adium/AIPlugin.h>
+#import <Foundation/Foundation.h>
+
+@interface AdiumFacebook : NSObject <AIPlugin>
+
+@end
---
+++ adium-facebook/AdiumFacebook.m
@@ -0,0 +1,13 @@
+//
+//  AdiumFacebook.m
+//  adium-facebook
+//
+//  Created by duxet on 08/08/15.
+//  Copyright (c) 2015 duxet. All rights reserved.
+//
+
+#import "AdiumFacebook.h"
+
+@implementation AdiumFacebook
+
+@end
